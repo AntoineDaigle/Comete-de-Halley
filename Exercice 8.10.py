5357a7127ae2a37fab253a5b124b47c7ee5c5bfa
Antoine Daigle <antoinedaigle2108@hotmail.com>
2023-04-19 12:27:21 -0400

    whoop whoop

--- Exercice 8.10.py
+++ Exercice 8.10.py
@@ -23,6 +23,7 @@ class runge_kutta:
         self.b = b 
         self.N = N 
         self.h = (self.b-self.a)/self.N
+        self.og_h = self.h
         self.cond_init = cond_init
         self.tpoints = np.arange(self.a,self.b,self.h)
 
@@ -49,76 +50,105 @@ class runge_kutta:
 
 
 
-    def h_prime(self, h, goal, i, j):
-        #optimus prime
-        return h * (30 * h * goal /abs(i-j))**0.25
-    
 
 
-    def runge_kutta(self, h):
-        k1 = h * self.f(self.cond_init)
-        k2 = h * self.f(self.cond_init+0.5*k1)
-        k3 = h * self.f(self.cond_init+0.5*k2)
-        k4 = h * self.f(self.cond_init+k3)
+    def runge_kutta(self, h, initial_conditions):
+        k1 = h * self.f(initial_conditions)
+        k2 = h * self.f(initial_conditions+0.5*k1)
+        k3 = h * self.f(initial_conditions+0.5*k2)
+        k4 = h * self.f(initial_conditions+k3)
         return k1, k2, k3, k4
         
 
 
     def simulation(self):
         """Méthode de Runge-Kutta."""
-        self.xpoints = []
-        self.vxpoints = []
-        self.ypoints = []
-        self.vypoints = []
-        self.zpoints = []
-        self.vzpoints = []
+        xpoints = []
+        vxpoints = []
+        ypoints = []
+        vypoints = []
+        zpoints = []
+        vzpoints = []
+        h_values = []
 
-        delta = 2000
-        # self.cond_ini
+        def rho(i, j):
+            eps_x = abs(i[0]-j[0])/30
+            eps_y = abs(i[2]-j[2])/30
+            eps_z = abs(i[4]-j[4])/30
+            return 30 * self.h * 1000 /np.sqrt(eps_x**2 + eps_y**2 + eps_z**2)
+        
+        def h_prime(rho):
+            return self.h * rho**0.25
+
 
         for t in tqdm(self.tpoints):
-            self.xpoints.append(self.cond_init[0])
-            self.vxpoints.append(self.cond_init[1])
+            xpoints.append(self.cond_init[0])
+            vxpoints.append(self.cond_init[1])
 
-            self.ypoints.append(self.cond_init[2])
-            self.vypoints.append(self.cond_init[3])
+            ypoints.append(self.cond_init[2])
+            vypoints.append(self.cond_init[3])
             
-            self.zpoints.append(self.cond_init[4])
-            self.vzpoints.append(self.cond_init[5])
+            zpoints.append(self.cond_init[4])
+            vzpoints.append(self.cond_init[5])
 
-            k1, k2, k3, k4 = self.runge_kutta(self.h)
-            temp_cond_in_normal = self.cond_init + (k1+2*k2+2*k3+k4)/6
+            # h + h
+            k1_h, k2_h, k3_h, k4_h = self.runge_kutta(self.h, self.cond_init)
+            cond_init_h = self.cond_init + (k1_h+2*k2_h+2*k3_h+k4_h)/6
 
-            k1, k2, k3, k4 = self.runge_kutta(2*self.h)
-            temp_cond_in_doublé = self.cond_init + (k1+2*k2+2*k3+k4)/6
-
-
-            # Prendre l'élément 0, 2, 4 pour les positions
-            h_prime = self.h_prime(self.h, 2000, temp_cond_in_normal, temp_cond_in_doublé)
+            k1_h_p_h, k2_h_p_h, k3_h_p_h, k4_h_p_h = self.runge_kutta(self.h, cond_init_h)            
+            cond_init_h_p_h = cond_init_h + (k1_h_p_h+2*k2_h_p_h+2*k3_h_p_h+k4_h_p_h)/6
 
 
+            # 2h
+            k1_2h, k2_2h, k3_2h, k4_2h = self.runge_kutta(2 * self.h, self.cond_init)
+            cond_init_2h = self.cond_init + (k1_2h+2*k2_2h+2*k3_2h+k4_2h)/6
+
+            # Calcul rho
+            p = rho(cond_init_h_p_h, cond_init_2h)
 
 
+            if p > 1:
+                k1, k2, k3, k4 = self.runge_kutta(self.h, self.cond_init)
+                self.cond_init += (k1+2*k2+2*k3+k4)/6
+                self.h = h_prime(p)
+                if self.h >=2*self.og_h:
+                    self.h = 2*self.og_h
+                # else:
+                #     self.h =1.0001*self.h
 
 
-        return self.tpoints, self.xpoints, self.vxpoints, self.ypoints, self.vypoints, self.zpoints, self.vzpoints
+            else:
+                h_pri = h_prime(p)
+                k1, k2, k3, k4 = self.runge_kutta(h_pri, self.cond_init)
+                self.cond_init += (k1+2*k2+2*k3+k4)/6
+                self.h = h_pri
+
+            h_values.append(self.h)
+
+
+        return self.tpoints, xpoints, vxpoints, ypoints, vypoints, zpoints, vzpoints, h_values
 
 
 
 if __name__ == "__main__":
-    t, x, vx, y, vy, z, vz = runge_kutta(0, 2e9, 100000, [4e12, 0, 0, 500, 0, 0]).simulation()
+    t, x, vx, y, vy, z, vz, h = runge_kutta(0, 2e9, 100000, [4e12, 0, 0, 500, 0, 0]).simulation()
 
-
-    fig = plt.figure()
-    ax = plt.axes()
-    # ax = plt.axes(projection='3d')
-
-    # ax.plot(t, x, label="x")
-    # ax.plot(t, y, label="y")
-    ax.plot(x, y)
-    ax.scatter(0, 0, c="red", label="Soleil")
-    ax.set_xlabel("x [m]")
-    ax.set_ylabel("y [m]")
-    # ax.set_zlabel("z [m]")
-    plt.legend()
+    plt.plot(t, h)
     plt.show()
+
+    plt.plot(x, y)
+    plt.show()
+
+    # fig = plt.figure()
+    # ax = plt.axes()
+    # # ax = plt.axes(projection='3d')
+
+    # # ax.plot(t, x, label="x")
+    # # ax.plot(t, y, label="y")
+    # ax.plot(x, y)
+    # ax.scatter(0, 0, c="red", label="Soleil")
+    # ax.set_xlabel("x [m]")
+    # ax.set_ylabel("y [m]")
+    # # ax.set_zlabel("z [m]")
+    # plt.legend()
+    # plt.show()
