1508c7fdb0fa7cb17972f7d6ad88deb9582bd757
AntoineDaigle <antoinedaigle2108@hotmail.com>
2023-04-25 21:57:52 -0400

    nouvelles conditions

---
+++ périhélie.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+from scipy.constants import astronomical_unit
+
+data = pd.read_csv("data_simulation.csv")
+
+
+date_initiale = datetime(year=1758, month=11, day=14)
+norme_halley = np.sqrt(data["x_halley"]**2 + data["y_halley"]**2 + data["z_halley"]**2 )
+
+
+dist_min = np.min(norme_halley)
+
+for i in range(len(norme_halley)):
+    if norme_halley[i] == dist_min:
+        loca = i
+        break
+
+
+date_périhélie = date_initiale + timedelta(seconds=data["time"][loca])
+print("La date du périhélie est le:", date_périhélie)
+
+temps = [date_initiale + timedelta(seconds=i) for i in data["time"]]
+
+
+
+plt.plot(temps, norme_halley/astronomical_unit)
+plt.scatter(date_périhélie, dist_min/astronomical_unit)
+plt.xlabel("Date [YYYY-MM]")
+plt.ylabel("Distance [au]")
+plt.title("Distance entre la comète et le soleil.")
+plt.tight_layout()
+plt.minorticks_on()
+plt.show()
+
+
+
+
+
+
--- vision_halley.py
+++ vision_halley.py
@@ -1,14 +1,36 @@
 import numpy as np
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
 
 
 data = pd.read_csv("data_simulation.csv")
 
-diff = (data["x_terre"]-data["x_halley"])**2
-print(diff)
+date_initiale = datetime(year=1758, month=11, day=14)
 
-distance_terre_comete = np.sqrt(1)
-distance_terre_soleil = 1
+projection = []
 
 
+for i in range(len(data["time"])):
+    d_tc = np.array([data["x_terre"][i]-data["x_halley"][i],
+                    data["y_terre"][i]-data["y_halley"][i],
+                    data["z_terre"][i]-data["z_halley"][i]])
+    d_ts = np.array([data["x_terre"][i],
+                    data["y_terre"][i],
+                    data["z_terre"][i]])
+    
+    
+    res_dot = np.dot(d_tc, d_ts)
+    if res_dot >= 0 :
+        projection.append(1)
+    else:
+        projection.append(0)
+
+
+temps = [date_initiale + timedelta(seconds=i) for i in data["time"]]
+plt.plot(temps, projection)
+plt.xlabel("Date [YYYY-MM]")
+plt.minorticks_on()
+plt.title("Observation possible de la comète de Halley.")
+plt.tight_layout()
+plt.show()
